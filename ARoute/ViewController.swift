075cc053919a7e20c3cdcb16eafdbdd4da4b38ff
edmund-h <447ght@gmail.com>
2018-11-06 10:45:36 -0500

    initial commit

---
+++ ARoute/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARoute
+//
+//  Created by Edmund Holderbaum on 11/6/18.
+//  Copyright © 2018 Dawn Trigger Enterprises. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
